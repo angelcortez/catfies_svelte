ace4d8bfd6c6403fc554632de1ea0aa17ab95865
aeco77 <angelcortez77@gmail.com>
2020-06-18 16:15:11 -0500

    first commit

---
+++ src/components/Footer.svelte
@@ -0,0 +1,14 @@
+<style>
+  .Footer {
+    font-size: 11px;
+    letter-spacing: 1px;
+    font-weight: normal;
+    color: #c7c7c7;
+  }
+</style>
+
+<div class="Footer">
+    <div class="Footer-container">  
+        <div class="Fotter-copy">@2020 Catfies From Ángel Cortéz</div>
+    </div>
+</div>
